cad5dcedb12e360ecc5f70781c9eb40697481c70
Ali.G <alimikael57@gmail.com>
2025-11-10 15:42:17 +0200

    add bucket

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,3 +11,8 @@ output "private_ip" {
   value = aws_instance.bastion_host.private_ip
   description = "Private IP of bastion host"
 }
+
+output "private_bucket_name" {
+  value = aws_s3_bucket.b["private"].bucket
+  description = "The name of your S3 bucket"
+}
