0bade043d027bb8c9a63beb52d1ba7873aa3d963
Ali.G <alimikael57@gmail.com>
2025-12-08 21:27:21 +0200

    add RDS instance for gitea

---
+++ terraform/backend.tf
@@ -0,0 +1,36 @@
+# RDS instance for gitea
+resource "aws_db_instance" "rds" {
+  db_name               = "giteadb"
+  identifier            = "gitea-remote-db"
+  allocated_storage     = 40
+  max_allocated_storage = 80
+
+  engine         = data.aws_rds_engine_version.postgres.engine
+  engine_version = data.aws_rds_engine_version.postgres.version
+  instance_class = var.rds_instance_class
+
+  username                    = "gitea"
+  manage_master_user_password = true
+
+  apply_immediately      = true
+  multi_az               = true
+  db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids = [aws_security_group.sg["dbserver"].id]
+  
+  tags = {
+    Name = "gitea-remote-db"
+  }
+}
+
+# Subnet group for RDS instance
+resource "aws_db_subnet_group" "db_subnets" {
+  name = "subnet-group"
+  subnet_ids = [
+    aws_subnet.s["private-data-subnet-1"].id,
+    aws_subnet.s["private-data-subnet-2"].id
+  ]
+
+  tags = {
+    Name = "db-subnet-group"
+  }
+}
