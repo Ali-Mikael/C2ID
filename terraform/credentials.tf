f930bbf8b9fc0ef1a0f4dfdea5b2be19e6eeaf47
Ali.G <alimikael57@gmail.com>
2025-12-08 21:33:11 +0200

    create credentials

---
+++ terraform/credentials.tf
@@ -0,0 +1,14 @@
+# Credentials
+# -----------
+
+# Admin user
+resource "aws_secretsmanager_secret" "admin" {
+  name = "admin"
+}
+
+# The secret
+# (Variable in >> secrets.tf (only locally tho, .gitignore is taking care of that ;))
+resource "aws_secretsmanager_secret_version" "v" {
+  secret_id     = aws_secretsmanager_secret.admin.id
+  secret_string = jsonencode(var.secret)
+}
