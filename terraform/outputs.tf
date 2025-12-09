83338b4ea326235cff182822e3a11f870e62b5e8
Ali.G <alimikael57@gmail.com>
2025-12-09 16:20:46 +0200

    add rds values

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,27 @@
-output "public_ip" {
-  value = aws_instance.bastion_host.public_ip
-  description = "Public IP of bastion host"
+# output "bastion_public_ip" {
+#   value = aws_instance.bastion_host.public_ip
+#   description = "Public IP of bastion host"
+# }
+
+output "test_instance_pub_ip"{
+  description = "Public IP of test instance"
+  value = aws_instance.test.public_ip
 }
 
-output "private_ip" {
-  value = aws_instance.bastion_host.private_ip
-  description = "Private IP of bastion host"
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.rds.address
+  sensitive   = true
 }
 
-output "private_bucket_name" {
-  value = aws_s3_bucket.b["private"].bucket
-  description = "The name of your S3 bucket"
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.rds.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.rds.username
+  sensitive   = true
 }
