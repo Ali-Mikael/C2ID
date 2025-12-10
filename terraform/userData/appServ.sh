2f0d1b454e4333ab12a8d2ed2e9dc83df8887633
Ali.G <alimikael57@gmail.com>
2025-12-10 16:20:42 +0200

    provision scripts for instances

---
+++ terraform/userData/appServ.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+
+# Get the gitea binaries
+wget -O gitea https://dl.gitea.com/gitea/1.25.2/gitea-1.25.2-linux-amd64
+chmod +x gitea
+
+# Create a user for gitea to run as
+adduser \
+   --system \
+   --shell /bin/bash \
+   --gecos 'Git Version Control' \
+   --group \
+   --disabled-password \
+   --home /home/git \
+   git
+
+# Create necessary directory structure for gitea
+mkdir -p /var/lib/gitea/{custom,data,log}
+chown -R git:git /var/lib/gitea/
+chmod -R 750 /var/lib/gitea/
+mkdir /etc/gitea
+chown root:git /etc/gitea
+chmod 770 /etc/gitea
+
+cp gitea /usr/local/bin/gitea
+
+# Create the service
+cat << 'EOF' > /etc/systemd/system/gitea.service
+[Unit]
+Description=Gitea (Git with a cup of tea)
+After=network.target
+Wants=postgresql.service
+After=postgresql.service
+
+[Service]
+# IF Repos with lots of files >> causing HTTP 500 error, uncomment next line.
+# LimitNOFILE=524288:524288
+RestartSec=2s
+Type=simple
+User=git
+Group=git
+WorkingDirectory=/var/lib/gitea/
+
+ExecStart=/usr/local/bin/gitea web --config /etc/gitea/app.ini
+Restart=always
+Environment=USER=git HOME=/home/git GITEA_WORK_DIR=/var/lib/gitea
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable gitea
+systemctl daemon-reload
+systemctl enable gitea --now
+
+# Then you go to >> http://<instance_ip>:3000
+# And finish the installation
+
+# When that's done >> SSH into instance && change the perms back to read only
+# chmod 750 /etc/gitea
+# chmod 640 /etc/gitea/app.ini
---
+++ terraform/userData/ciServ.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+
+apt install -y apt-transport-https fontconfig openjdk-21-jre
+
+apt install fontconfig openjdk-21-jre
+
+wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt update
+
+apt install jenkins
