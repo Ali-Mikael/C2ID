cd09816e924fbcc925a43caee0141fe5c3eccdd7
Ali.G <alimikael57@gmail.com>
2025-12-10 23:53:45 +0200

    update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,30 +10,30 @@ output "FrontEndALB" {
 
 output "WebServer1" {
   description = "Web server private IP"
-  value       = aws_instance.web_server_1.private_ip
+  value       = aws_instance.web_server[0].private_ip
 }
 
 output "AppServer1" {
   description = "App server private IP"
-  value       = aws_instance.app_server_1.private_ip
+  value       = aws_instance.app_server[0].private_ip
 }
 
 output "CiServer1" {
   description = "CI server private IP"
-  value       = aws_instance.ci_server_1.private_ip
+  value       = aws_instance.ci_server[0].private_ip
 }
 
 output "WebServer2" {
   description = "Web server private IP"
-  value       = aws_instance.web_server_2.private_ip
+  value       = aws_instance.web_server[1].private_ip
 }
 
 output "AppServer2" {
   description = "App server private IP"
-  value       = aws_instance.app_server_2.private_ip
+  value       = aws_instance.app_server[1].private_ip
 }
 
 output "CiServer2" {
   description = "CI server private IP"
-  value       = aws_instance.ci_server_2.private_ip
+  value       = aws_instance.ci_server[1].private_ip
 }
