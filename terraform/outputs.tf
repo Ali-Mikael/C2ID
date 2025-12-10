32a0beb284f5d40947a3ab6c97bf62db18c5ce42
Ali.G <alimikael57@gmail.com>
2025-12-10 22:05:54 +0200

    add outputs for new instances

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,22 +3,37 @@ output "BastionHost" {
   description = "Public IP of bastion host"
 }
 
-output "WebServer" {
-  description = "Web server private IP"
-  value       = aws_instance.web_server.private_ip
-}
-
-output "AppServer" {
-  description = "App server private IP"
-  value       = aws_instance.app_server.private_ip
-}
-
-output "CiServer" {
-  description = "CI server private IP"
-  value       = aws_instance.ci_server.private_ip
-}
-
 output "FrontEndALB" {
   description = "ALB DNS name"
   value       = aws_lb.frontend.dns_name
+}
+
+output "WebServer1" {
+  description = "Web server private IP"
+  value       = aws_instance.web_server_1.private_ip
+}
+
+output "AppServer1" {
+  description = "App server private IP"
+  value       = aws_instance.app_server_1.private_ip
+}
+
+output "CiServer1" {
+  description = "CI server private IP"
+  value       = aws_instance.ci_server_1.private_ip
+}
+
+output "WebServer2" {
+  description = "Web server private IP"
+  value       = aws_instance.web_server_2.private_ip
+}
+
+output "AppServer2" {
+  description = "App server private IP"
+  value       = aws_instance.app_server_2.private_ip
+}
+
+output "CiServer2" {
+  description = "CI server private IP"
+  value       = aws_instance.ci_server_2.private_ip
 }
