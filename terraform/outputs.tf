89862d12e8e847dca9af25fde490cd15c4b5ecfa
Ali.G <alimikael57@gmail.com>
2025-12-10 21:11:21 +0200

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,31 +1,24 @@
-output "bastion_public_ip" {
-  value = aws_instance.bastion_host.public_ip
+output "BastionHost" {
+  value       = aws_instance.bastion_host.public_ip
   description = "Public IP of bastion host"
 }
 
-output "webserv_priv_ip" {
+output "WebServer" {
   description = "Web server private IP"
   value       = aws_instance.web_server.private_ip
 }
 
-output "appserv_priv_ip" {
+output "AppServer" {
   description = "App server private IP"
   value       = aws_instance.app_server.private_ip
 }
 
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.rds.address
-  # sensitive   = true
+output "CiServer" {
+  description = "CI server private IP"
+  value       = aws_instance.ci_server.private_ip
 }
 
-output "rds_username" {
-  description = "RDS instance root username"
-  value       = aws_db_instance.rds.username
-  sensitive   = true
-}
-
-output "lb_dns_name" {
+output "FrontEndALB" {
   description = "ALB DNS name"
-  value = aws_lb.frontend.dns_name
+  value       = aws_lb.frontend.dns_name
 }
