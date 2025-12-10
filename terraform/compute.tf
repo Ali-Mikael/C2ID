7498865544b15522cf0dff19df76569ac4c4361b
Ali.G <alimikael57@gmail.com>
2025-12-10 19:57:27 +0200

    add custom AMI option

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -54,3 +54,33 @@ resource "aws_key_pair" "bastion_host" {
 #   description = "Public IP of test instance"
 #   value       = aws_instance.test.public_ip
 # }
+
+# -*- Uncomment if you want to create a custom AMI from an instance
+# Just specify the instance ID you want to use! -*- 
+# ------------------------------------------------>
+# resource "aws_ami_from_instance" "custom_ami" {
+#   name               = "Custom AMI"
+#   source_instance_id = aws_instance.vm1.id
+#   timeouts {
+#     create = "10m"
+#   }
+# }
+
+# -*- Uncomment to create VM from that AMI -*-
+# ------------------------------------------->
+# resource "aws_instance" "custom_vm" {
+#   ami                         = aws_ami_from_instance.custom_ami.id
+#   instance_type               = var.instance_type
+#   subnet_id                   = aws_subnet.s["public-1"].id
+#   key_name                    = aws_key_pair.vm1.key_name
+#   vpc_security_group_ids      = [aws_security_group.instance_sg.id]
+
+#   tags = {
+#     Name = "customVM"
+#   }
+# }
+# output "customVM_public_ip" {
+#   value = aws_instance.custom_vm.public_ip
+#   description = "Public IP of custom VM"
+# }
+# <----------------------------------------
