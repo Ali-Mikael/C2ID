518d944921eb0648be751192da17c2bbed0e9abb
Ali.G <alimikael57@gmail.com>
2025-12-11 19:43:44 +0200

    fix CIserver subnet

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "app_server" {
   key_name      = aws_key_pair.instance.key_name
   user_data     = file("./userData/appServ.sh")
 
-  # Forcing private IP (easier to config reverse proxy) ->
+  # Forcing private IP makes configuring Nginx easier by way of init script
   private_ip = count.index == 0 ? cidrhost(local.subnets.private-app-subnet-1, 100) : cidrhost(local.subnets.private-app-subnet-2, 100)
 
   vpc_security_group_ids = [
@@ -93,13 +93,40 @@ resource "aws_instance" "jenkins_runner" {
   count         = 2
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.s["private-data-subnet-${count.index + 1}"].id
+  subnet_id     = aws_subnet.s["private-app-subnet-${count.index + 1}"].id
   key_name      = aws_key_pair.instance.key_name
   user_data     = file("./userData/runner.sh")
 
-  vpc_security_group_ids = [aws_security_group.sg["runner"].id]
+  # Jenkins controller connects via SSH, so the admin SG will suffice. 
+  # The SG only accepts SSH connections from within the VPC already.
+  vpc_security_group_ids = [aws_security_group.sg["admin"].id]
 
   tags = {
     Name = "jenkins-runner-${count.index + 1}"
   }
+}
+
+# -*- Redis ElastiCache -*-
+# -------------------------
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "gitea-cache"
+  engine               = "redis"
+  parameter_group_name = "default.redis7"
+  node_type            = var.redis_node_type
+  engine_version       = var.redis_engine_version
+  num_cache_nodes      = 1 # <- A must for Redis
+  apply_immediately    = true
+
+  port               = local.port.redis
+  security_group_ids = [aws_security_group.sg["redis"].id]
+  subnet_group_name  = aws_elasticache_subnet_group.sg.id
+}
+
+# Subnet group for elasticache
+resource "aws_elasticache_subnet_group" "sg" {
+  name = "elasticache-subnet-group"
+  subnet_ids = [
+    aws_subnet.s["private-data-subnet-1"].id,
+    aws_subnet.s["private-data-subnet-2"].id
+  ]
 }
