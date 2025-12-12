cdea7c0c58f71e222330fb41be472ac45e246615
Ali.G <alimikael57@gmail.com>
2025-12-12 04:09:03 +0200

    change ALB to classic ELB due to alb not having support for proxy protocol

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -2,52 +2,34 @@
 # -*- Frontend -*-
 # ----------------
 
-# -*- ALB -*-
-# -----------
-resource "aws_lb" "frontend" {
-  name               = "frontend-alb"
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg["alb"].id]
-  subnets = [
-    aws_subnet.s["public-subnet-1"].id,
-    aws_subnet.s["public-subnet-2"].id
-  ]
+# -*- Load Balancer -*-
+# ---------------------
+resource "aws_elb" "frontend" {
+  name               = "frontend-load-balancer"
+  subnets            = [aws_subnet.s["public-subnet-1"].id, aws_subnet.s["public-subnet-2"].id]
+
+  listener {
+    instance_port     = local.port.http
+    instance_protocol = "http"
+    lb_port           = local.port.http
+    lb_protocol       = "http"
+  }
+  idle_timeout    = 120
+  instances       = [aws_instance.web_server[0].id, aws_instance.web_server[1].id]
+  security_groups = [aws_security_group.sg["lb"].id]
 
   tags = {
-    Name     = "frontend-alb"
-    Endpoint = "public-facing"
+    Name = "frontend-load-balancer"
   }
 }
 
-# ALB target group
-resource "aws_lb_target_group" "frontend" {
-  name     = "web-target-group"
-  port     = local.port.http
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+# Enable proxy protocol
+resource "aws_proxy_protocol_policy" "proxy" {
+  load_balancer  = aws_elb.frontend.name
+  instance_ports = ["${local.port.http}"]
 }
 
-# ALB target group attachment
-resource "aws_lb_target_group_attachment" "web" {
-  count = 2
-  target_group_arn = aws_lb_target_group.frontend.arn
-  target_id        = aws_instance.web_server[count.index].id
-  port             = local.port.http
-}
-
-# ALB listener
-resource "aws_lb_listener" "frontend" {
-  load_balancer_arn = aws_lb.frontend.arn
-  port              = local.port.http
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend.arn
-  }
-}
-
-# -*- Web server -*-
+# -*- Web Server -*-
 # ------------------
 resource "aws_instance" "web_server" {
   count         = 2
@@ -55,7 +37,7 @@ resource "aws_instance" "web_server" {
   instance_type = var.instance_type
   subnet_id     = aws_subnet.s["private-web-subnet-${count.index + 1}"].id
   key_name      = aws_key_pair.instance.key_name
-  user_data     = templatefile("userData/nginx_init.sh.tftpl", {
+  user_data = templatefile("userData/nginx_init.sh.tftpl", {
     app_server_private_ip = count.index == 0 ? cidrhost(local.subnets.private-app-subnet-1, 100) : cidrhost(local.subnets.private-app-subnet-2, 100)
   })
 
