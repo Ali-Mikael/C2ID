55b2c3f203b377c170b037d6e41061f57fe3d2f5
Ali.G <alimikael57@gmail.com>
2025-12-12 04:03:46 +0200

    create last required step for SG to actually be secure

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -47,54 +47,61 @@ locals {
 # --------------
 locals {
   ip_all = "0.0.0.0/0"
-  # my_ip = "****/32" # <- Specify your IP/network and use with bastion SG. Principle of least privledge!
+  # my_ip = "..../." # <- Specify your IP and use with Bastion SG. Principle of least privledge!
 
+  # NOTE: -Potato_version_1.deluxe-
+  # Very rigid design and doesn't scale well. Also hard to maintain.  
+  # Will fix this when I get the time
   security_groups = {
-
-    instance = {
+    test = {
       allow_http  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = local.ip_all }
       allow_https = { from_port = local.port.https, to_port = local.port.https, cidr_ipv4 = local.ip_all }
     }
 
     bastion = {
-      allow_ssh   = { from_port = local.port.ssh, to_port = local.port.ssh, cidr_ipv4 = local.ip_all }
+      allow_ssh = { from_port = local.port.ssh, to_port = local.port.ssh, cidr_ipv4 = local.ip_all }
     }
 
     # Attach to an instance => enable SSH access from within VPC
     admin = {
-      allow_ssh_from_vpc = { from_port = local.port.ssh, to_port = local.port.ssh, cidr_ipv4 = var.main_cidr }
+      allow_ssh_from_bastion = { from_port = local.port.ssh, to_port = local.port.ssh, cidr_ipv4 = local.subnets.public-subnet-1 }
     }
 
     webserver = {
-      allow_http  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = var.main_cidr },
-      allow_https = { from_port = local.port.https, to_port = local.port.https, cidr_ipv4 = var.main_cidr }
+      allow_http_pub1  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = local.subnets.public-subnet-1 },
+      allow_http_pub2  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = local.subnets.public-subnet-2 },
+      allow_https_pub1 = { from_port = local.port.https, to_port = local.port.https, cidr_ipv4 = local.subnets.public-subnet-1 }
+      allow_https_pub2 = { from_port = local.port.https, to_port = local.port.https, cidr_ipv4 = local.subnets.public-subnet-2 }
     }
 
     appserver = {
-      allow_http  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = var.main_cidr }
-      allow_gitea = { from_port = local.port.gitea, to_port = local.port.gitea, cidr_ipv4 = var.main_cidr }
+      allow_http_web1  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = local.subnets.private-web-subnet-1 }
+      allow_http_web2  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = local.subnets.private-web-subnet-2 }
+      allow_gitea_web1 = { from_port = local.port.gitea, to_port = local.port.gitea, cidr_ipv4 = local.subnets.private-web-subnet-1 }
+      allow_gitea_web2 = { from_port = local.port.gitea, to_port = local.port.gitea, cidr_ipv4 = local.subnets.private-web-subnet-2 }
     }
 
     ciserver = {
-      allow_jenkins = { from_port = local.port.jenkins, to_port = local.port.jenkins, cidr_ipv4 = var.main_cidr }
+      allow_jenkins_app1 = { from_port = local.port.jenkins, to_port = local.port.jenkins, cidr_ipv4 = local.subnets.private-app-subnet-1 }
+      allow_jenkins_app2 = { from_port = local.port.jenkins, to_port = local.port.jenkins, cidr_ipv4 = local.subnets.private-app-subnet-2 }
     }
 
     dbserver = {
-      allow_db = { from_port = local.port.postgres, to_port = local.port.postgres, cidr_ipv4 = var.main_cidr }
+      allow_db_app1 = { from_port = local.port.postgres, to_port = local.port.postgres, cidr_ipv4 = local.subnets.private-app-subnet-1 }
+      allow_db_app2 = { from_port = local.port.postgres, to_port = local.port.postgres, cidr_ipv4 = local.subnets.private-app-subnet-2 }
     }
 
-    redis = {
-      allow_redis = { from_port = local.port.redis, to_port = local.port.redis, cidr_ipv4 = var.main_cidr }
+    cacheserver = {
+      allow_redis_app1 = { from_port = local.port.redis, to_port = local.port.redis, cidr_ipv4 = local.subnets.private-app-subnet-1 }
+      allow_redis_app2 = { from_port = local.port.redis, to_port = local.port.redis, cidr_ipv4 = local.subnets.private-app-subnet-2 }
     }
 
-    alb = {
+    lb = {
       allow_http  = { from_port = local.port.http, to_port = local.port.http, cidr_ipv4 = local.ip_all },
       allow_https = { from_port = local.port.https, to_port = local.port.https, cidr_ipv4 = local.ip_all },
-
     }
   }
-}
-locals {
+
   # Preparing SGs for rule creation
   sg_rules = flatten([
     for sg, rules in local.security_groups : [
