e299f268172f0777d9a732c36b786700769bea0f
Ali.G <alimikael57@gmail.com>
2025-12-12 04:14:07 +0200

    new script skeleton in place (not in use), tf managing DB pword for now and moved the RDS resource around. Also: Name change for cache SG

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,96 @@
 # ---------------------
-# -*- Backend magic -*-
+# -*- Backend Magic -*-
 # ---------------------
 
 
-# -*- RDS instance for gitea -*-
+# -*- App Servers -*-
+# -------------------
+#      (Gitea)
+resource "aws_instance" "app_server" {
+  count         = 2
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.s["private-app-subnet-${count.index + 1}"].id
+  key_name      = aws_key_pair.instance.key_name
+  user_data     = file("./userData/gitea_init.sh")
+  #   templatefile("userData/app_init.sh.tftpl", { # <- As soon as the script starts working we will revert to this. In the mean time the OG will do!
+  #     alb_dns        = "${aws_elb.frontend.dns_name}",
+  #     db_endpoint    = "${aws_db_instance.rds.endpoint}",
+  #     db_password    = "${random_password.db.result}",
+  #     db_user        = "${var.db_user}",
+  #     redis_endpoint = "${aws_elasticache_cluster.app.cache_nodes.0.address}:${aws_elasticache_cluster.app.cache_nodes.0.port}",
+  #     key            = "${random_bytes.key.base64}",
+  #     token          = "${random_bytes.token.base64}",
+  #     web_subnet     = count.index == 0 ? "${local.subnets.private-web-subnet-1}" : "${local.subnets.private-web-subnet-2}"
+  #   })
+
+  # Forcing private IP makes configuring Nginx easier by way of init script
+  private_ip = count.index == 0 ? cidrhost(local.subnets.private-app-subnet-1, 100) : cidrhost(local.subnets.private-app-subnet-2, 100)
+
+  vpc_security_group_ids = [
+    aws_security_group.sg["appserver"].id,
+    aws_security_group.sg["admin"].id
+  ]
+
+  tags = {
+    Name = "app-server-${count.index + 1}"
+  }
+}
+
+# Gitea secret_key
+resource "random_bytes" "key" {
+  length = 128
+}
+# Gitea internal_token
+resource "random_bytes" "token" {
+  length = 128
+}
+#DB psswd
+resource "random_password" "db" {
+  length = 12
+}
+
+# -*- CI Servers -*-
+# ------------------
+#     (Jenkins)
+resource "aws_instance" "ci_server" {
+  count         = 2
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.s["private-app-subnet-${count.index + 1}"].id
+  key_name      = aws_key_pair.instance.key_name
+  user_data     = file("./userData/ciServ.sh")
+
+  vpc_security_group_ids = [
+    aws_security_group.sg["ciserver"].id,
+    aws_security_group.sg["admin"].id
+  ]
+
+  tags = {
+    Name = "ci-server-${count.index + 1}"
+  }
+}
+
+# -*- Jenkins Runners -*-
+# -----------------------
+resource "aws_instance" "jenkins_runner" {
+  count         = 2
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.s["private-app-subnet-${count.index + 1}"].id
+  key_name      = aws_key_pair.instance.key_name
+  user_data     = file("./userData/runner.sh")
+
+  # Jenkins controller connects via SSH, so the admin SG will suffice. 
+  # The SG only accepts SSH connections from within the VPC already.
+  vpc_security_group_ids = [aws_security_group.sg["admin"].id]
+
+  tags = {
+    Name = "jenkins-runner-${count.index + 1}"
+  }
+}
+
+# -*- RDS instance for Gitea -*-
 # ------------------------------
 resource "aws_db_instance" "rds" {
   db_name               = "giteadb"
@@ -15,8 +102,8 @@ resource "aws_db_instance" "rds" {
   engine_version = data.aws_rds_engine_version.postgres.version
   instance_class = var.rds_instance_class
 
-  username                    = "gitea"
-  manage_master_user_password = true
+  username = var.db_user
+  password = random_password.db.result
 
   apply_immediately      = true
   skip_final_snapshot    = true
@@ -42,73 +129,9 @@ resource "aws_db_subnet_group" "db_subnets" {
   }
 }
 
-# -*- App servers -*-
-# -------------------
-#      (Gitea)
-resource "aws_instance" "app_server" {
-  count         = 2
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.s["private-app-subnet-${count.index + 1}"].id
-  key_name      = aws_key_pair.instance.key_name
-  user_data     = file("./userData/appServ.sh")
-
-  # Forcing private IP makes configuring Nginx easier by way of init script
-  private_ip = count.index == 0 ? cidrhost(local.subnets.private-app-subnet-1, 100) : cidrhost(local.subnets.private-app-subnet-2, 100)
-
-  vpc_security_group_ids = [
-    aws_security_group.sg["appserver"].id,
-    aws_security_group.sg["admin"].id
-  ]
-
-  tags = {
-    Name = "app-server-${count.index + 1}"
-  }
-}
-
-# -*- CI servers -*-
-# ------------------
-#     (Jenkins)
-resource "aws_instance" "ci_server" {
-  count         = 2
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.s["private-app-subnet-${count.index + 1}"].id
-  key_name      = aws_key_pair.instance.key_name
-  user_data     = file("./userData/ciServ.sh")
-
-  vpc_security_group_ids = [
-    aws_security_group.sg["ciserver"].id,
-    aws_security_group.sg["admin"].id
-  ]
-
-  tags = {
-    Name = "ci-server-${count.index + 1}"
-  }
-}
-
-# -*- Jenkins runners -*-
-# -----------------------
-resource "aws_instance" "jenkins_runner" {
-  count         = 2
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.s["private-app-subnet-${count.index + 1}"].id
-  key_name      = aws_key_pair.instance.key_name
-  user_data     = file("./userData/runner.sh")
-
-  # Jenkins controller connects via SSH, so the admin SG will suffice. 
-  # The SG only accepts SSH connections from within the VPC already.
-  vpc_security_group_ids = [aws_security_group.sg["admin"].id]
-
-  tags = {
-    Name = "jenkins-runner-${count.index + 1}"
-  }
-}
-
 # -*- Redis ElastiCache -*-
 # -------------------------
-resource "aws_elasticache_cluster" "redis" {
+resource "aws_elasticache_cluster" "app" {
   cluster_id           = "gitea-cache"
   engine               = "redis"
   parameter_group_name = "default.redis7"
@@ -118,7 +141,7 @@ resource "aws_elasticache_cluster" "redis" {
   apply_immediately    = true
 
   port               = local.port.redis
-  security_group_ids = [aws_security_group.sg["redis"].id]
+  security_group_ids = [aws_security_group.sg["cacheserver"].id]
   subnet_group_name  = aws_elasticache_subnet_group.sg.id
 }
 
